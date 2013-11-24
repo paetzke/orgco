4425095ef0f62c1a2c3dd1d06fad0018368347ad
Friedrich Paetzke <f.paetzke@gmail.com>
2013-11-24 13:48:45 +0100

    Add shortcut for converting org to html

--- orgco/__init__.py
+++ orgco/__init__.py
@@ -5,3 +5,15 @@ Copyright (c) 2013, Friedrich Paetzke (f.paetzke@gmail.com)
 All rights reserved.
 
 """
+from orgco.convert import convert
+from orgco.orgalyzer import OrgDoc
+
+
+def _convert(content, outputtye):
+    orgdoc = OrgDoc(content)
+    result = convert(orgdoc, outputtye)
+    return result
+
+
+def convert_html(content):
+    return _convert(content, 'html')
