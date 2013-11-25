b87da757ddbf932373b40b1a28e6ceaa84a1c23f
Friedrich Paetzke <f.paetzke@gmail.com>
2013-11-25 09:30:21 +0100

    Release version 0.0.3

--- setup.py
+++ setup.py
@@ -5,13 +5,23 @@ Copyright (c) 2013, Friedrich Paetzke (f.paetzke@gmail.com)
 All rights reserved.
 
 """
+import os
 from distutils.core import setup
+
+from orgco import convert_rst
+
+
+pathname = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(pathname, 'README.org')) as f:
+    long_description = convert_rst(f.read())
 
 
 setup(name='orgco',
-      description='',
-      version='0.0.1.0',
+      description='orgco is tool/library for converting emacs` orgmode to other formats.',
+      long_description=long_description,
+      version='0.0.3',
       author='Friedrich Paetzke',
       author_email='f.paetzke@gmail.com',
+      license='BSD',
       url='https://github.com/paetzke/orgco',
       packages=['orgco'])
