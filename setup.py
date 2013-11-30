c01bb985771e9e1385da44a0fcf310e72ee97b49
Friedrich Paetzke <f.paetzke@gmail.com>
2013-12-01 00:24:57 +0100

    Release version 0.0.4

--- setup.py
+++ setup.py
@@ -8,18 +8,25 @@ All rights reserved.
 import os
 from distutils.core import setup
 
-from orgco import convert_rst
-
 
 pathname = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(pathname, 'README.org')) as f:
-    long_description = convert_rst(f.read())
+try:
+    from orgco import convert_rst
+
+    with open(os.path.join(pathname, 'README.org')) as org_file:
+        with open(os.path.join(pathname, 'README.rst'), 'w') as rst_file:
+            rst_file.write(convert_rst(org_file.read()))
+except (FileNotFoundError, ImportError):
+    pass
+
+with open(os.path.join(pathname, 'README.rst')) as f:
+    long_description = f.read()
 
 
 setup(name='orgco',
       description='orgco is tool/library for converting emacs` orgmode to other formats.',
       long_description=long_description,
-      version='0.0.3',
+      version='0.0.4',
       author='Friedrich Paetzke',
       author_email='f.paetzke@gmail.com',
       license='BSD',
