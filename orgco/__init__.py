2a0bd24b0bf9d2a424101b8c5a1557bfdbea6417
Friedrich Paetzke <paetzke@fastmail.fm>
2015-08-15 12:31:16 +0200

    Bump version: 0.1.1 → 0.2.0

--- orgco/__init__.py
+++ orgco/__init__.py
@@ -9,7 +9,7 @@ All rights reserved.
 from .convert import convert
 from .orgalyzer import OrgDoc
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__ = 'Friedrich Paetzke'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2013 Friedrich Paetzke'
